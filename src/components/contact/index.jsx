c7365e710fa8fbb64f5c172d4a1b84476fc101ac
Amit <amitaxh612@gmail.com>
2024-03-16 00:49:21 +0530

    map

--- src/components/contact/index.jsx
+++ src/components/contact/index.jsx
@@ -160,7 +160,7 @@ const Contact = () => {
       {/* <!-- GOOGLE MAP AREA START --> */}
       <div className="google-map mb-120">
         <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d9334.271551495209!2d-73.97198251485975!3d40.668170674982946!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c25b0456b5a2e7%3A0x68bdf865dda0b669!2sBrooklyn%20Botanic%20Garden%20Shop!5e0!3m2!1sen!2sbd!4v1590597267201!5m2!1sen!2sbd"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3889.040357507816!2d77.60936387527002!3d12.905126516331809!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae151ddca75827%3A0x12e1b004e5480a85!2sStarLive%20PG!5e0!3m2!1sen!2sin!4v1710527038250!5m2!1sen!2sin"
           width="100%"
           height="100%"
         ></iframe>
